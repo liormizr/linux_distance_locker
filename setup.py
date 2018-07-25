4b6f4c5e626e0b3fe435d175b29dd460cad9fadc
lior <li.mizr@gmail.com>
2018-07-26 01:42:16 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(
     name='distance_locker',
@@ -8,7 +7,8 @@ setup(
     url='https://github.com/liormizr/linux_distance_locker',
     author='Lior Mizrahi',
     author_email='li.mizr@gmail.com',
-    packages=find_packages(),
+    py_modules=['distance_locker'],
+    scripts=['bluetooth_distance_sensor.sh'],
     entry_points={
         'console_scripts': [
             'distance_locker = distance_locker:main',
