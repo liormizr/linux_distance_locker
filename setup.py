a8b79c4be01a148779189d6aab7c2077b186878d
lior <li.mizr@gmail.com>
2018-07-26 18:46:05 +0300

    add readme

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='distance_locker',
     version='0.0.1',
@@ -10,6 +13,8 @@ setup(
     author_email='li.mizr@gmail.com',
     packages=find_packages(),
     data_files=[('bin', ['bluetooth_distance_sensor.sh'])],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'distance_locker = distance_locker.__main__:main',
